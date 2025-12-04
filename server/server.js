0beae5724a77a64306690e545c54e314c75b1aea
LaurenIsLearning <pleasecontactlauren@gmail.com>
2025-12-04 13:30:39 -0500

    class version and where i ended

--- server/server.js
+++ server/server.js
@@ -15,7 +15,63 @@ const app = express();
 
 //TODO Implement missing structures and functionality
 //DB connection, middleware, and API routes
+//DB Connection-----------------
+mongoose.connect(dbURL, {
+  useNewURLParser: true,
+  userUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error(err));
 
+//middleware--------------------
+app.use(cors());
+app.use(express.json());
+
+///mongoose schema + model------------------
+const animalSchema = new mongoose.Schema ({
+  animal: String,
+  url: String
+})
+
+const Animal = mongoose.model("Animal", animalSchema);
+
+//API ---------------------------------
+// POST /animals/upload
+app.post ("/amimals/upload", async (req, res) => {
+  const {name, pictureUrl } = req.body;
+
+  //save to db
+  await Animal.create({
+    animal: name,
+    url: pictureUrl
+  });
+  
+  res.send(`Animal ${name} uploaded!`);
+})
+
+//GET /amimals/searc/:animal
+app.get("/animals/search/:amimal", async (req, res) => {
+  const search = req.params.animal;
+  const matches = await Animal.find({
+    animal: { $regex: new RegExp(`^${search}$`, "i") }
+  });
+
+  res.json(matches);
+})
+
+//DELETE /animals/clear
+app.delete("/animals/clear", async (req, res) => {
+  await Animal.deleteMany({});
+  res.json({ message: "All animals deleted."});
+});
+
+//serv react build
+app.use(express.static(buildPath));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+})
+
+//start server
 const port = 8080;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,82 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+console.log("THIS IS THE CORRECT APP.JSX");
 
 function App() {
   //Any needed hooks
   //TODO
-  
+  // state
+  const [result, setResult] = useState("");
+  const [imgSrc, setImgSrc] = useState("");
+
+  //**************************** */
+  useEffect(()=>{
+    setTimeout(() => {
+      setResult("");
+    }, 3000)
+  }, [result]);
+  //**************************** */
   //Functions for API
   //upload
-  async function upload(e){
+  async function upload(e) {
     e.preventDefault();
 
     //TODO
+    //read values directly
+    const animal = document.getElementById("name").value;
+    const url = document.getElementById("picture").value;
+
+    //send to backend
+    const res = await fetch("/animals/upload", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: e.target.name.value, 
+        pictureUrl: e.target.picture.value })
+    });
+
+    if (res.status == 200){
+      const result = await res.text();
+      setResult(result);
+    }
+
+    // //cypress check msg string
+    // const data = await res.json();
+    // setResult(data.message);
   }
 
   //search
-  async function search(e){
+  async function search(e) {
     e.preventDefault();
 
     //TODO
+    const query = document.querySelector("[data-testid='query']").value;
+
+    const res = await fetch(`/animals/search/${query}`);
+    const data = await res.json();
+
+    // if not results, clear image
+    if (data.length > 0) {
+      setImgSrc(data[0].url);
+    } else {
+      setImgSrc("");
+    }
   }
 
   //clear
-  async function clear(e){
+  async function clear(e) {
 
     //TODO
+    const res = await fetch("/animals/clear", {
+      method: "DELETE"
+    });
+
+    const data = await res.json();
+
+    //cypress checks this
+    setResult(data.message);
+    //clear img UI
+    setImgSrc("");
 
   }
 
@@ -33,14 +87,14 @@ function App() {
       <form onSubmit={upload}>
         <label htmlFor="name">Animal:</label>
         <input type="text" name="name" id="name" placeholder='Name'
-        data-testid="animal"
+          data-testid="animal"
         />
         <label htmlFor="picture">Picture Address:</label>
         <input type="text" name="picture" id="picture"
-        data-testid="url"
+          data-testid="url"
         />
         <button type='submit'
-        data-testid="upload"
+          data-testid="upload"
         >Upload</button>
       </form>
 
@@ -48,22 +102,22 @@ function App() {
       <form onSubmit={search}>
         <label htmlFor="name">Name:</label>
         <input type="text" name="name" id="name"
-        data-testid="query"
+          data-testid="query"
         />
         <button type='submit'
-        data-testid="search"
+          data-testid="search"
         >Search</button>
       </form>
 
       <p>
-        <b data-testid="result">{/* TODO */}</b>
+        <b data-testid="result">{/* TODO */ result}</b>
       </p>
       <section>
-        <img src={/* TODO */} height={200} alt="Animal Image"
-        data-testid="image"
+        <img src={/* TODO */ imgSrc} height={200} alt="Animal Image"
+          data-testid="image"
         />
       </section>
-      
+
       <button onClick={clear} data-testid="clear">Clear Animal Database</button>
 
     </div>
@@ -73,4 +127,3 @@ function App() {
 export default App;
 
 //npm run dev to start React app and Express server
-
